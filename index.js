fb5ae6dcb464edd72415c4522b805a2c419f51a9
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-24 03:44:06 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name]
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats]
+    return newCats;
+}
+
+function removeLastCat(name) {
+    let newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+
+
+function removeFirstCat(name) {
+    let newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+}
